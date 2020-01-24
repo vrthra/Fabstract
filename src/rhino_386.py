caf76395c8e4a468854d8289c899b1448ecf4a9c
Rahul <rahul@gopinath.org>
2020-01-24 19:39:29 +0000

    add last of rhino

---
+++ src/rhino_386.py
@@ -0,0 +1,29 @@
+import Abstract as A
+import Infra as I
+from Abstract import PRes
+import json
+import os
+import os.path
+import tempfile
+
+def my_predicate(src):
+    with tempfile.NamedTemporaryFile(prefix='rhino') as tmp:
+        tname = tmp.name
+        tmp.write(src.encode('UTF-8'))
+        tmp.flush()
+        o = A.do('java -jar lang/js/compilers/rhino-1.7.7.2.jar'.split(' ') + [tmp.name])
+        if o.returncode == 0: return PRes.failed
+        out = o.stdout.decode()
+        if 'java.lang.NullPointerException' in out:
+            return PRes.success
+        elif 'ERROR - Parse error' in out:
+            return PRes.invalid
+        elif 'TIMEOUT' in out:
+            # timeout should be failed.
+            return PRes.failed
+        return PRes.failed
+
+
+import sys
+if __name__ == '__main__':
+    I.main('./lang/js/grammar/javascript.fbjson', './lang/js/bugs/rhino.385.js', my_predicate, max_checks=100)
