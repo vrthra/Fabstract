c24ead57940f42f38a39e0463db5f2b7e71e60c7
Rahul Gopinath <rahul@gopinath.org>
2020-01-24 19:25:29 +0000

    add clojure

---
+++ src/clojure_2092.py
@@ -0,0 +1,28 @@
+import Abstract as A
+import Infra as I
+from Abstract import PRes
+import json
+import os
+import os.path
+import tempfile
+
+def my_predicate(src):
+    with tempfile.NamedTemporaryFile(prefix='clojure') as tmp:
+        tname = tmp.name
+        tmp.write(src.encode('UTF-8'))
+        tmp.flush()
+        o = A.do('java -jar lang/clojure/compilers/clojure.jar'.split(' ') + [tmp.name])
+        if o.returncode == 0: return PRes.failed
+        out = o.stdout.decode()
+        if 'Syntax error (IllegalArgumentException)' in out and 'No matching field found:' in out:
+            return PRes.success
+        elif 'Syntax error compiling' in out:
+            return PRes.invalid
+        elif 'TIMEOUT' in out:
+            # timeout should be failed.
+            return PRes.failed
+        return PRes.failed
+
+import sys
+if __name__ == '__main__':
+    I.main('./lang/clojure/grammar/clojure.fbjson', './lang/clojure/bugs/clj-2092.clj', my_predicate, max_checks=100)
---
+++ src/clojure_2345.py
@@ -0,0 +1,28 @@
+import Abstract as A
+import Infra as I
+from Abstract import PRes
+import json
+import os
+import os.path
+import tempfile
+
+def my_predicate(src):
+    with tempfile.NamedTemporaryFile(prefix='clojure') as tmp:
+        tname = tmp.name
+        tmp.write(src.encode('UTF-8'))
+        tmp.flush()
+        o = A.do('java -jar clojure/compilers/clojure.jar'.split(' ') + [tmp.name])
+        if o.returncode == 0: return PRes.failed
+        out = o.stdout.decode()
+        if 'Syntax error (VerifyError)' in out and 'Catch type is not a subclass of Throwable in exception handler 6' in out:
+            return PRes.success
+        elif 'Syntax error compiling' in out:
+            return PRes.invalid
+        elif 'TIMEOUT' in out:
+            # timeout should be failed.
+            return PRes.failed
+        return PRes.failed
+
+import sys
+if __name__ == '__main__':
+    I.main('./lang/clojure/grammar/clojure.fbjson', './lang/clojure/bugs/clj-2345.clj', my_predicate, max_checks=100)
