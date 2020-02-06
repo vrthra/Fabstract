23f0c32246921e11a608f11e6294be475bb15362
Rahul Gopinath <rahul@gopinath.org>
2020-02-06 17:20:35 +0000

    update

--- src/fuzz_clojure_2473.py
+++ src/fuzz_clojure_2473.py
@@ -1,5 +1,5 @@
 import Fuzz as F
-import clojure_2743 as Main
+import clojure_2473 as Main
 
 if __name__ == '__main__':
-    F.main('./lang/clojure/grammar/clojure.fbjson', './lang/clojure/bugs/clj-2743.clj', Main.my_predicate)
+    F.main('./lang/clojure/grammar/clojure.fbjson', './lang/clojure/bugs/clj-2473.clj', Main.my_predicate)
