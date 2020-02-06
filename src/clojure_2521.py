abc5852ad0c50bd792689c16d54a3d91a7290452
Rahul Gopinath <rahul@gopinath.org>
2020-02-06 12:34:20 +0000

    clojure

--- src/clojure_2521.py
+++ src/clojure_2521.py
@@ -1,26 +1,17 @@
-import Abstract as A
 import Infra as I
 from Abstract import PRes
-import json
-import os
-import os.path
-import tempfile
 
 def my_predicate(src):
-    with tempfile.NamedTemporaryFile(prefix='clojure') as tmp:
-        tname = tmp.name
-        tmp.write(src.encode('UTF-8'))
-        tmp.flush()
-        o = A.do('java -jar lang/clojure/compilers/clojure.jar'.split(' ') + [tmp.name])
-        if o.returncode == 0: return PRes.failed
-        out = o.stdout.decode()
-        if 'Syntax error (ClassFormatError)' in out:
-            return PRes.success
-        elif 'Syntax error compiling' in out:
-            return PRes.invalid
-        elif 'TIMEOUT' in out:
-            return PRes.invalid
-        return PRes.failed
+    o = I.do('clojure', 'java -jar lang/clojure/compilers/clojure.jar', src)
+    if o.returncode == 0: return PRes.failed
+    out = o.stdout
+    if 'Syntax error (ClassFormatError)' in out:
+        return PRes.success
+    elif 'Syntax error compiling' in out:
+        return PRes.invalid
+    elif 'TIMEOUT' in out:
+        return PRes.invalid
+    return PRes.failed
 
 import sys
 if __name__ == '__main__':
