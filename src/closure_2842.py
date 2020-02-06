b42c33b84998b23ff3204427252aeb402e8743ab
Rahul Gopinath <rahul@gopinath.org>
2020-02-06 12:48:48 +0000

    closure

--- src/closure_2842.py
+++ src/closure_2842.py
@@ -1,26 +1,17 @@
-import Abstract as A
 import Infra as I
 from Abstract import PRes
-import json
-import os
-import os.path
-import tempfile
 
 def my_predicate(src):
-    with tempfile.NamedTemporaryFile(prefix='closure') as tmp:
-        tname = tmp.name
-        tmp.write(src.encode('UTF-8'))
-        tmp.flush()
-        o = A.do('java -jar lang/js/compilers/closure-compiler-v20200101.jar'.split(' ') + [tmp.name])
-        if o.returncode == 0: return PRes.failed
-        out = o.stdout.decode()
-        if 'java.lang.RuntimeException: INTERNAL COMPILER ERROR.' in out:
-            return PRes.success
-        elif 'ERROR - Parse error' in out:
-            return PRes.invalid
-        elif 'TIMEOUT' in out:
-            return PRes.invalid
-        return PRes.failed
+    o = I.do('closure', 'java -jar lang/js/compilers/closure-compiler-v20200101.jar', src)
+    if o.returncode == 0: return PRes.failed
+    out = o.stdout
+    if 'java.lang.RuntimeException: INTERNAL COMPILER ERROR.' in out:
+        return PRes.success
+    elif 'ERROR - Parse error' in out:
+        return PRes.invalid
+    elif 'TIMEOUT' in out:
+        return PRes.invalid
+    return PRes.failed
 
 import sys
 if __name__ == '__main__':
