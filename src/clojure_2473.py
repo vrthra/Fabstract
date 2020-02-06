47f07c96b4155b6175ba62f8114cd5deaf4b297a
Rahul Gopinath <rahul@gopinath.org>
2020-02-06 12:38:21 +0000

    update

--- src/clojure_2473.py
+++ src/clojure_2473.py
@@ -5,9 +5,13 @@ def my_predicate(src):
     o = I.do('clojure', 'java -jar lang/clojure/compilers/clojure.jar', src)
     if o.returncode == 0: return PRes.failed
     out = o.stdout
-    if 'Syntax error macroexpanding clojure.core/let' in out and ':user/keys - failed:' in out:
+    if 'Syntax error macroexpanding clojure.core/let' in out and \
+        "simple-symbol? at: [:bindings :form :map-destructure :map-binding 0 :local-symbol] spec: :clojure.core.specs.alpha/local-name" in out and \
+        ":user/keys - failed: simple-symbol? at: [:bindings :form :map-destructure :map-binding 0 :local-symbol] spec: :clojure.core.specs.alpha/local-name" in out:
         return PRes.success
     elif 'Syntax error compiling' in out:
+        return PRes.invalid
+    elif 'Syntax error reading source' in out:
         return PRes.invalid
     elif 'TIMEOUT' in out:
         return PRes.invalid
