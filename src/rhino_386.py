c89e0341be7588df681bc1168828efca81324407
Rahul Gopinath <rahul@gopinath.org>
2020-02-06 11:39:20 +0000

    rhino

--- src/rhino_386.py
+++ src/rhino_386.py
@@ -1,26 +1,19 @@
-import Abstract as A
 import Infra as I
 from Abstract import PRes
-import json
-import os
-import os.path
-import tempfile
 
 def my_predicate(src):
-    with tempfile.NamedTemporaryFile(prefix='rhino') as tmp:
-        tname = tmp.name
-        tmp.write(src.encode('UTF-8'))
-        tmp.flush()
-        o = A.do('java -jar lang/js/compilers/rhino-1.7.7.2.jar'.split(' ') + [tmp.name])
-        if o.returncode == 0: return PRes.failed
-        out = o.stdout.decode()
-        if 'java.lang.NullPointerException' in out:
-            return PRes.success
-        elif 'ERROR - Parse error' in out:
-            return PRes.invalid
-        elif 'TIMEOUT' in out:
-            return PRes.invalid
-        return PRes.failed
+    o = I.do('rhino', 'java -jar lang/js/compilers/rhino-1.7.7.2.jar')
+    o = I.do('lua', './lang/lua/compilers/lua --', src)
+    if o.returncode == 0: return PRes.failed
+    if o.returncode == -11: return PRes.success
+    out = o.stdout
+    if 'java.lang.NullPointerException' in out:
+        return PRes.success
+    elif 'ERROR - Parse error' in out:
+        return PRes.invalid
+    elif 'TIMEOUT' in out:
+        return PRes.invalid
+    return PRes.failed
 
 
 import sys
